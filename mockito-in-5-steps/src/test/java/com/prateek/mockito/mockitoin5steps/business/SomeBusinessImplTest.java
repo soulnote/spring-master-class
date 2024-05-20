e61cb4366125f0a942c8f5b71819b1ed15b07e41
soulnote <dataouter@gmail.com>
2024-05-20 20:32:03 +0530

    Done with stubs moving to mocks

--- mockito-in-5-steps/src/test/java/com/prateek/mockito/mockitoin5steps/business/SomeBusinessImplTest.java
+++ mockito-in-5-steps/src/test/java/com/prateek/mockito/mockitoin5steps/business/SomeBusinessImplTest.java
@@ -7,22 +7,40 @@ import org.junit.jupiter.api.Test;
 class SomeBusinessImplTest {
 
 	@Test
-	void test() {
-		DataServiceStub dataServiceStub = new DataServiceStub();
-		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceStub);
+	void findTheGreatestFromAllDatabasicScenerio() {
+		DataService dataServiceStub1 = new DataServiceStub1();
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceStub1);
 		int result = businessImpl.findTheGreatestFromAllData();
 		assertEquals(34, result);
+	}
+	
+	@Test
+	void findTheGreatestFromAllDataWithOneValue() {
+		DataService dataServiceStub2 = new DataServiceStub2();
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceStub2);
+		int result = businessImpl.findTheGreatestFromAllData();
+		assertEquals(24, result);
 	}
 
 }
 
 
-class DataServiceStub implements DataService{
+class DataServiceStub1 implements DataService{
 
 	@Override
 	public int[] retrieveAllData() {
 		// TODO Auto-generated method stub
 		return new int[] {24, 23 , 34};
+	}
+	
+}
+
+class DataServiceStub2 implements DataService{
+
+	@Override
+	public int[] retrieveAllData() {
+		// TODO Auto-generated method stub
+		return new int[] {24};
 	}
 	
 }
